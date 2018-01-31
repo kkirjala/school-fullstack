5a3a0dff08c05b393496df42b85f47589a9c519d
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-01-31 13:22:25 +0200

    Functionality implemented (tasks 2.1-2.4)

--- osa2_courses/src/index.js
+++ osa2_courses/src/index.js
@@ -1,52 +1,123 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Osa = (props) => <p>{props.osa} {props.tehtavia}</p>
-const Otsikko = (props) => <h1>{props.kurssi.nimi}</h1>
-const Sisalto = (props) => {
-  const [osa1, osa2, osa3] = props.kurssi.osat
-  return(
-    <div>
-      <Osa osa={osa1.nimi} tehtavia={osa1.tehtavia} />
-      <Osa osa={osa2.nimi} tehtavia={osa2.tehtavia} />
-      <Osa osa={osa3.nimi} tehtavia={osa3.tehtavia} />
-    </div>
-  )
-}
-const Yhteensa = (props) => {
-  const [osa1, osa2, osa3] = props.kurssi.osat
-  
-  return(
-    <p>yhteensä {osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} tehtävää</p>
-  )
-}
+
+
+
 
 const App = () => {
-  const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14
-      }
+    const courses = [
+        {
+        courseName: 'Half Stack -sovelluskehitys',
+        id: 1,
+        courseParts: [
+                {
+                    partName: 'Reactin perusteet',
+                    assignments: 10,
+                    id: 1
+                },
+                {
+                    partName: 'Tiedonvälitys propseilla',
+                    assignments: 7,
+                    id: 2
+                },
+                {
+                    partName: 'Komponenttien tila',
+                    assignments: 14,
+                    id: 3
+                }
+            ]
+        },
+        {
+        courseName: 'Node.js',
+        id: 2,
+        courseParts: [
+                {
+                    partName: 'Routing',
+                    assignments: 3,
+                    id: 1
+                },
+                {
+                    partName: 'Middlewaret',
+                    assignments: 7,
+                    id: 2
+                }
+            ]
+        }
     ]
-  }
-  return (
-    <div>
-      <Otsikko kurssi={kurssi}/>
-      <Sisalto kurssi={kurssi} />
-      <Yhteensa kurssi={kurssi}  />
-    </div>
-  )
+  
+
+    const courseList = courses.map((course) => {
+
+        // remove spaces + partid = unique element key
+        const courseKey = course.courseName.replace(/\s/g, '') + course.id
+
+        return (
+            <Course key={courseKey} course={course} />
+        )
+    })
+
+
+    return (
+      <div>
+        {courseList}
+      </div>
+    )
 }
+
+
+const Course = ({course}) => {
+
+    return (
+        <div>
+            <CourseHeader course={course} />
+            <CourseContent course={course} />
+            <CourseTotalAssignments course={course} />
+        </div>
+    )
+
+}
+
+const CourseContent = ({course}) => {
+
+    const courseParts = course.courseParts.map((part) => {
+
+        // remove spaces + partid = unique element key
+        const partKey = part.partName.replace(/\s/g, '') + part.id
+
+        return (
+            <CoursePart 
+                key={partKey}
+                partName={part.partName} 
+                assignments={part.assignments} 
+            />
+        )
+
+    });
+
+    return(
+      <div>
+        {courseParts}
+      </div>
+    )
+}
+
+const CourseTotalAssignments = ({course}) => {
+    
+    const amountAssignments = course.courseParts
+        .map((part) => part.assignments)
+        .reduce((acc, curr) => acc + curr)
+    
+    return(
+      <p>yhteensä {amountAssignments} tehtävää</p>
+    )
+}
+
+
+
+const CourseHeader = ({course}) => <h1>{course.courseName}</h1>
+const CoursePart = ({partName, assignments}) => <p>{partName} {assignments}</p>
+
 
 ReactDOM.render(
   <App />,
