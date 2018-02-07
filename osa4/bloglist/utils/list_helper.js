58fe32a08f9dcd0a91efdd78313d55a876be2219
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-02-07 12:52:27 +0200

    mostBlogs function and unit test (task 4.6)

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -27,9 +27,34 @@ const favoriteBlog = (blogs) => {
 
 }
 
+const mostBlogs = (blogs) => {
+	if (blogs == null || blogs.length == 0) {
+		return null
+	}
+
+	const authorsArray = blogs.map(element => element.author)
+
+	const mostOccuringAuthor = authorsArray
+		.sort((a, b) => 
+			authorsArray.filter(current => current === a).length
+			- authorsArray.filter(current => current === b).length
+		)
+		.pop()
+
+	const amountBlogsByAuthor = blogs
+		.filter(element => element.author === mostOccuringAuthor)
+		.length
+
+	const author = { author: mostOccuringAuthor, blogs: amountBlogsByAuthor }
+	return author
+	// return { mostOccuringAuthor, amountBlogsByAuthor }
+
+}
+
   
 module.exports = {
 	dummy,
 	totalLikes,
-	favoriteBlog
+	favoriteBlog,
+	mostBlogs
 }
