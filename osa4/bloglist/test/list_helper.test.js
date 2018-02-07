98a252883c8e8f34c0a796db577a4c0ff42b5088
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-02-07 22:05:17 +0200

    Removed copy-paste code from tests

--- osa4/bloglist/test/list_helper.test.js
+++ osa4/bloglist/test/list_helper.test.js
@@ -97,16 +97,8 @@ describe('most likes by author', () => {
 	})
 
 	test('a list with one item returns the correct author', () => {
-		const singleList = [
-			{
-				_id: '5a422a851b54a676234d17f7',
-				title: 'React patterns',
-				author: 'Michael Chan',
-				url: 'https://reactpatterns.com/',
-				likes: 7,
-				__v: 0
-			}
-		]
+		const singleList = []
+		singleList.push(testHelper.initialBlogs[0])
 
 		const result = listHelper.mostLikes(singleList)
 		expect(result).toEqual({'author': 'Michael Chan', 'likes': 7})
@@ -114,58 +106,7 @@ describe('most likes by author', () => {
 	})
 
 	test('multiple blog posts returns the correct author', () => {
-		const multipleBlogs = [
-			{
-				_id: '5a422a851b54a676234d17f7',
-				title: 'React patterns',
-				author: 'Michael Chan',
-				url: 'https://reactpatterns.com/',
-				likes: 7,
-				__v: 0
-			},
-			{
-				_id: '5a422aa71b54a676234d17f8',
-				title: 'Go To Statement Considered Harmful',
-				author: 'Edsger W. Dijkstra',
-				url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-				likes: 5,
-				__v: 0
-			},
-			{
-				_id: '5a422b3a1b54a676234d17f9',
-				title: 'Canonical string reduction',
-				author: 'Edsger W. Dijkstra',
-				url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-				likes: 12,
-				__v: 0
-			},
-			{
-				_id: '5a422b891b54a676234d17fa',
-				title: 'First class tests',
-				author: 'Robert C. Martin',
-				url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-				likes: 10,
-				__v: 0
-			},
-			{
-				_id: '5a422ba71b54a676234d17fb',
-				title: 'TDD harms architecture',
-				author: 'Robert C. Martin',
-				url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-				likes: 0,
-				__v: 0
-			},
-			{
-				_id: '5a422bc61b54a676234d17fc',
-				title: 'Type wars',
-				author: 'Robert C. Martin',
-				url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-				likes: 2,
-				__v: 0
-			}  
-		]
-
-		const result = listHelper.mostLikes(multipleBlogs)
+		const result = listHelper.mostLikes(testHelper.initialBlogs)
 		expect(result).toEqual({'author': 'Edsger W. Dijkstra', 'likes': 17})
 
 	})
