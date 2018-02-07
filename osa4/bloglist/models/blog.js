4a1fd4e1a4c3e7aac1cb0c50b476686957863fa9
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-02-07 10:27:34 +0200

    Blog model separated to it's own module (task 4.2)

---
+++ osa4/bloglist/models/blog.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+const mongoUrl = process.env.MONGODB_URI
+
+mongoose.connect(mongoUrl)
+mongoose.Promise = global.Promise
+
+const blogSchema = mongoose.Schema(
+    {
+        title: String,
+        author: String,
+        url: String,
+        likes: Number
+      }
+)
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog
