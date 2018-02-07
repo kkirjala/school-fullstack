334eedf7491eca3cb72d29323b6409633caa965c
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-02-07 13:35:00 +0200

    mostLikes functionality and unit test (task 4.7)

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -47,7 +47,40 @@ const mostBlogs = (blogs) => {
 
 	const author = { author: mostOccuringAuthor, blogs: amountBlogsByAuthor }
 	return author
-	// return { mostOccuringAuthor, amountBlogsByAuthor }
+
+}
+
+const mostLikes = (blogs) => {
+
+	if (blogs == null || blogs.length == 0) {
+		return null
+	}
+	
+	const authors = blogs.map(blog => blog.author)
+
+	const mostLikedAuthor = authors
+		.sort((a, b) => {
+			return (
+				blogs
+					.filter(blog => blog.author === a) // only author A
+					.map(blog => blog.likes) // build an array with likes by author A
+					.reduce((total, currBlog) => { return total + currBlog }, 0) // calculate total for author A
+				- blogs // compare with equal calculation results of author B
+					.filter(blog => blog.author === b)
+					.map(blog => blog.likes)
+					.reduce((total, currBlog) => { return total + currBlog }, 0)
+			)
+		})
+		.pop()
+
+	// TODO: remove copy-paste (see above)
+	const amountLikesByAuthor = blogs
+		.filter(blog => blog.author === mostLikedAuthor)
+		.map(blog => blog.likes)
+		.reduce((total, currBlog) => { return total + currBlog }, 0)
+
+	const author = { author: mostLikedAuthor, likes: amountLikesByAuthor }
+	return author
 
 }
 
@@ -56,5 +89,6 @@ module.exports = {
 	dummy,
 	totalLikes,
 	favoriteBlog,
-	mostBlogs
+	mostBlogs,
+	mostLikes
 }
