3c35cd3bba753525b304ed4bbaf391b34f1a4cfd
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-02-07 10:33:18 +0200

    Blog controller separated to own module (task 4.2)

--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -4,43 +4,12 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-
-require('dotenv').config()
-const mongoUrl = process.env.MONGODB_URI
-
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
+const blogsRouter = require('./controllers/blogs.js')
 
 app.use(cors())
 app.use(bodyParser.json())
 
-mongoose.connect(mongoUrl)
-mongoose.Promise = global.Promise
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
