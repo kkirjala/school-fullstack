8eca67e5c0f1a2d8ff01d560418575e6cf38f082
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-02-07 15:00:15 +0200

    updated eslint config (console-logging allowed)

--- osa4/bloglist/.eslintrc.js
+++ osa4/bloglist/.eslintrc.js
@@ -20,7 +20,8 @@ module.exports = {
         "semi": [
             "error",
             "never"
-        ]
+        ],
+        "no-console": "off"
     },
     "globals": {
         "test": true,
