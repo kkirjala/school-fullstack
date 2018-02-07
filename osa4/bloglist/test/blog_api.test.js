7f0b8a18ca49f07b0a7ac902db4d8454df032203
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-02-07 22:03:04 +0200

    Fixed a bunch of ESlint errors

--- osa4/bloglist/test/blog_api.test.js
+++ osa4/bloglist/test/blog_api.test.js
@@ -7,157 +7,157 @@ const Blog = require('../models/blog')
 
 beforeAll(async () => {
 
-    await Blog.remove({}) // empty the DB
+	await Blog.remove({}) // empty the DB
 
-    const blogs = testHelper.initialBlogs.map(blog => new Blog(blog))
-    const promiseArray = blogs.map(blog => blog.save()) // all promises in one
-    await Promise.all(promiseArray)
+	const blogs = testHelper.initialBlogs.map(blog => new Blog(blog))
+	const promiseArray = blogs.map(blog => blog.save()) // all promises in one
+	await Promise.all(promiseArray)
 })
 
 describe('get blogs', () => {
 
-    test('Blog posts are fetched. Correct amount and content type.', async () => {
+	test('Blog posts are fetched. Correct amount and content type.', async () => {
 
-        const initialBlogs = await testHelper.blogsInDb()
+		const initialBlogs = await testHelper.blogsInDb()
 
-        const response = await api
-            .get('/api/blogs')
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
+		const response = await api
+			.get('/api/blogs')
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
 
-        expect(response.body.length).toBe(initialBlogs.length)
+		expect(response.body.length).toBe(initialBlogs.length)
 
-        })
+	})
 
 })
 
 describe('create blogs', () => {
 
-    test('Creating a new blog post', async () => {
+	test('Creating a new blog post', async () => {
 
-        const initialBlogs = await testHelper.blogsInDb()
+		const initialBlogs = await testHelper.blogsInDb()
 
-        await api
-            .post('/api/blogs')
-            .send({ title: 'Subject', author: 'BlogTester', url: 'http://localhost', likes: 7 })
-            .expect(201)
-            .expect('Content-Type', /application\/json/)
+		await api
+			.post('/api/blogs')
+			.send({ title: 'Subject', author: 'BlogTester', url: 'http://localhost', likes: 7 })
+			.expect(201)
+			.expect('Content-Type', /application\/json/)
            
-        const currentBlogs = await testHelper.blogsInDb()
+		const currentBlogs = await testHelper.blogsInDb()
 
-        expect(currentBlogs.length).toBe(initialBlogs.length + 1)
+		expect(currentBlogs.length).toBe(initialBlogs.length + 1)
 
-        const author = currentBlogs.pop().author
+		const author = currentBlogs.pop().author
 
-        expect(author).toBe('BlogTester')
+		expect(author).toBe('BlogTester')
 
-    })
+	})
 
-    test('Creating a new blog post with no likes', async () => {
+	test('Creating a new blog post with no likes', async () => {
 
-        const initialBlogs = await testHelper.blogsInDb()
+		const initialBlogs = await testHelper.blogsInDb()
 
-        await api
-            .post('/api/blogs')
-            .send({ title: 'Subject', author: 'BlogTester', url: 'http://localhost' })
-            .expect(201)
-            .expect('Content-Type', /application\/json/)
+		await api
+			.post('/api/blogs')
+			.send({ title: 'Subject', author: 'BlogTester', url: 'http://localhost' })
+			.expect(201)
+			.expect('Content-Type', /application\/json/)
             
-        const currentBlogs = await testHelper.blogsInDb()
+		const currentBlogs = await testHelper.blogsInDb()
 
-        expect(currentBlogs.length).toBe(initialBlogs.length + 1)
+		expect(currentBlogs.length).toBe(initialBlogs.length + 1)
 
-        const latestBlog = currentBlogs.pop()
+		const latestBlog = currentBlogs.pop()
 
-        expect(latestBlog.likes).toBe(0)
+		expect(latestBlog.likes).toBe(0)
 
-    })
+	})
 
-    test('Creating a new blog without title and url', async () => {
-        const initialBlogs = await testHelper.blogsInDb()
+	test('Creating a new blog without title and url', async () => {
+		const initialBlogs = await testHelper.blogsInDb()
 
-        await api
-            .post('/api/blogs')
-            .send({ author: 'BlogTester', likes: 10 })
-            .expect(400)
+		await api
+			.post('/api/blogs')
+			.send({ author: 'BlogTester', likes: 10 })
+			.expect(400)
 
-        const currentBlogs = await testHelper.blogsInDb()
+		const currentBlogs = await testHelper.blogsInDb()
 
-        expect(currentBlogs.length).toBe(initialBlogs.length)
+		expect(currentBlogs.length).toBe(initialBlogs.length)
         
-    })
+	})
 
 })
 
 describe('delete blogs', () => {
 
-    test('Deleting a blog post with valid ID', async () => {
+	test('Deleting a blog post with valid ID', async () => {
 
-        const initialBlogs = await testHelper.blogsInDb()
+		const initialBlogs = await testHelper.blogsInDb()
 
-        const deleteBlog = initialBlogs[Math.floor(initialBlogs.length / 2)] // pick a blog from middle
+		const deleteBlog = initialBlogs[Math.floor(initialBlogs.length / 2)] // pick a blog from middle
 
-        await api
-            .delete(`/api/blogs/${deleteBlog._id}`)
-            .expect(204)
+		await api
+			.delete(`/api/blogs/${deleteBlog._id}`)
+			.expect(204)
 
-        const currentBlogs = await testHelper.blogsInDb()
+		const currentBlogs = await testHelper.blogsInDb()
 
-        expect(currentBlogs.length).toBe(initialBlogs.length - 1)
+		expect(currentBlogs.length).toBe(initialBlogs.length - 1)
         
-    })
+	})
 
-    test('Deleting a blog post with invalid ID', async () => {
+	test('Deleting a blog post with invalid ID', async () => {
 
-        const falsifiedId = await testHelper.nonExistingId()
-        const initialBlogs = await testHelper.blogsInDb()
+		const falsifiedId = await testHelper.nonExistingId()
+		const initialBlogs = await testHelper.blogsInDb()
 
-        await api
-            .delete(`/api/blogs/${falsifiedId}`)
-            .expect(404)
+		await api
+			.delete(`/api/blogs/${falsifiedId}`)
+			.expect(404)
 
-        const currentBlogs = await testHelper.blogsInDb()
+		const currentBlogs = await testHelper.blogsInDb()
 
-        expect(currentBlogs.length).toBe(initialBlogs.length)
+		expect(currentBlogs.length).toBe(initialBlogs.length)
         
-    })
+	})
 
 })
 
 describe('modify blogs', () => {
 
-    test('Modifying blog author and likes', async () => {
+	test('Modifying blog author and likes', async () => {
 
-        const initialBlogs = await testHelper.blogsInDb()
+		const initialBlogs = await testHelper.blogsInDb()
 
-        const originalBlog = initialBlogs[Math.floor(initialBlogs.length / 2)] // pick a blog from middle
+		const originalBlog = initialBlogs[Math.floor(initialBlogs.length / 2)] // pick a blog from middle
 
-        const modifiedBlog = {
-            title: originalBlog.title,
-            author: 'modified author',
-            url: originalBlog.url,
-            likes: originalBlog.likes + 666
-        }
+		const modifiedBlog = {
+			title: originalBlog.title,
+			author: 'modified author',
+			url: originalBlog.url,
+			likes: originalBlog.likes + 666
+		}
 
-        await api
-            .put(`/api/blogs/${originalBlog._id}`)
-            .send(modifiedBlog)
-            .expect(204)
+		await api
+			.put(`/api/blogs/${originalBlog._id}`)
+			.send(modifiedBlog)
+			.expect(204)
 
-        const currentBlogs = await testHelper.blogsInDb()
+		const currentBlogs = await testHelper.blogsInDb()
 
-        const comparedBlog = currentBlogs[Math.floor(initialBlogs.length / 2)]
+		const comparedBlog = currentBlogs[Math.floor(initialBlogs.length / 2)]
 
-        expect(currentBlogs.length).toBe(initialBlogs.length)
-        expect(comparedBlog.title).toBe(originalBlog.title)
-        expect(comparedBlog.author).toBe('modified author')
-        expect(comparedBlog.url).toBe(originalBlog.url)
-        expect(comparedBlog.likes).toBe(originalBlog.likes + 666)
+		expect(currentBlogs.length).toBe(initialBlogs.length)
+		expect(comparedBlog.title).toBe(originalBlog.title)
+		expect(comparedBlog.author).toBe('modified author')
+		expect(comparedBlog.url).toBe(originalBlog.url)
+		expect(comparedBlog.likes).toBe(originalBlog.likes + 666)
         
-    })
+	})
 
 })
 
 afterAll(() => {
-  server.close()
+	server.close()
 })
