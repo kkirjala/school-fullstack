0a75c318b05fcd8bcfd38acca109cb97c868e6a8
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-03-08 09:40:12 +0200

    Added PropTypes for Togglable component (task 5.11)

--- osa5/bloglist-frontend/src/components/Togglable.js
+++ osa5/bloglist-frontend/src/components/Togglable.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class Togglable extends React.Component {
     constructor(props) {
@@ -27,6 +28,10 @@ class Togglable extends React.Component {
         </div>
       )
     }
+}
+
+Togglable.propTypes = {
+    buttonLabel: PropTypes.string.isRequired
 }
 
 export default Togglable
