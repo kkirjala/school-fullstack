fd5f23e11dbefe373e9bae4e00c8c9a484b37d59
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-03-09 18:57:38 +0200

    Action creators (task 6.2)

--- osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
@@ -1,29 +1,30 @@
 import React from 'react'
+import { voteRegistration } from '../reducers/anecdoteReducer'
 
 class AnecdoteList extends React.Component {
-  render() {
-    const anecdotes = this.props.store.getState()
-    return (
-      <div>
-        <h2>Anecdotes</h2>
-        {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
-            <div>
+	render() {
+		const anecdotes = this.props.store.getState()
+		return (
+			<div>
+				<h2>Anecdotes</h2>
+				{anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+					<div key={anecdote.id}>
+						<div>
+							{anecdote.content}
+						</div>
+						<div>
               has {anecdote.votes}
-              <button onClick={() => 
-                this.props.store.dispatch({ type: 'VOTE', id: anecdote.id })
-              }>
+							<button onClick={() => 
+								this.props.store.dispatch(voteRegistration(anecdote.id))
+							}>
                 vote
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
-    )
-  }
+							</button>
+						</div>
+					</div>
+				)}
+			</div>
+		)
+	}
 }
 
 export default AnecdoteList
