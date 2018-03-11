1da603a5efedb397793789804c80c24404f02cfe
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-03-11 14:39:21 +0200

    Grid layout for about page (task 6.22)

--- osa6/routed-anecdotes/src/App.js
+++ osa6/routed-anecdotes/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
-import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import { ListGroup, ListGroupItem, Grid, Row, Col, Image } from 'react-bootstrap'
 
 const Menu = (state, addNew, notification) => {
 
@@ -116,22 +116,38 @@ const Notification = ({ notification }) => {
     <div style={notificationStyle}>{notification}</div>
   )
 
-  }
-
+}
 
 
 const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
-      An anecdote is "a story with a point."</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
+<Grid>
+  <Row className="show-grid">
+    <Col>
+      <h2>About anecdote app</h2>
+    </Col>
+  </Row>
+  <Row className="show-grid">
+    <Col xs={12} md={8}>
+      <div>
+      
+        <p>According to Wikipedia:</p>
+        
+        <em>An anecdote is a brief, revealing account of an individual person or an incident. 
+          Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
+          such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
+          An anecdote is "a story with a point."</em>
+
+        <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+      </div>
+    </Col>
+    <Col xs={6} md={4}>
+      <div>
+        <Image src="/Linus_Torvalds.jpeg" rounded responsive />
+      </div>
+    </Col>
+  </Row>
+</Grid>
 )
 
 const Footer = () => (
